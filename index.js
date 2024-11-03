e307a7dd17a1d8b9e01bc6a52c6a65c873cc3e79
Nico Lozano <homeronicolaslozano@gmail.com>
2024-11-02 20:25:48 -0400

    working fetch

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-document.addEventListener("DOMContentLoaded", () => console.log("we are connected!!!"))
+document.addEventListener("DOMContentLoaded", () => fetchData)
+function fetchData () {
+    fetch('url') 
+    .then((resp) => resp.json())
+    .then((data) => console.log(data))
+}
